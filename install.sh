7dcb9ab6eb607c3100956d65b12272ff4dcd4d2b
foxcoder-0805 <eliseylomtev@gmail.com>
2026-05-12 18:24:31 +0300

    added install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo pacman -S nvim kitty fish zeditor flameshot gimp;
+sudo pacman -S vlc libreoffice zellij obsidian qbittorrent;
+sudo pacman -S bat cat lsd zoxide cowsay fastfetch;
+sudo flatpak install zoom; sudo flatpak install steam;
+
+git clone https://aur.archlinux.org/yay.git;cd yay;
+makepkg -si;
+
+yay -S yt-dlp;
+
+sudo pacman -R nano top;
+
+chsh -s $(which fish);
+
+rm ~/.config/fish/config.fish;
+rm ~/.config/kitty/kitty.conf;
+rm ~/.config/nvim/init.vim;
+
+cp cfg/fish/config.fish ~/.config/fish/config.fish;
+cp cfg/kitty/kitty.conf ~/.config/kitty/kitty.conf;
+cp cfg/nvim/init.vim ~/.config/nvim/init.vim;
+
+cowsay "Installation complete";
