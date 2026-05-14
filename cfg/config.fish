4def82158e01a6fbb3a0838ad5893bb95be63442
foxcoder-0805 <eliseylomtev@gmail.com>
2026-05-14 15:16:52 +0300

    update cfg/config.fish

--- cfg/config.fish
+++ cfg/config.fish
@@ -1,14 +1,17 @@
 alias n='nvim'
 alias g='git'
 alias c='g++'
-alias zap='sudo bash /home/foxcoder/zapret/service.sh'
+alias zap='bash /home/foxcoder/zapret/service.sh'
 alias p='sudo pacman'
+alias fl='sudo flatpak'
 alias lsd='lsd -a'
 alias ls='lsd -a'
 alias fsh='nvim /home/foxcoder/.config/fish/config.fish'
 alias kt='nvim /home/foxcoder/.config/kitty/kitty.conf'
 alias zed='zeditor'
 alias zel='zellij'
+alias cd='z'
+alias nn='nvim ~/.config/nvim/init.vim'
 
 zoxide init fish | source
 
