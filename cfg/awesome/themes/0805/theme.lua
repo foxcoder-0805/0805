8354282e545d2ff9c8daa41ad3c850d5ce376c02
foxcoder-0805 <eliseylomtev@gmail.com>
2026-06-17 16:26:53 +0300

    added awesome cfgs

---
+++ cfg/awesome/themes/0805/theme.lua
@@ -0,0 +1,36 @@
+local theme = {}
+
+-- Font
+theme.font          = "JetBrainsMono Nerd Font 16"
+
+-- Colors
+theme.bg_normal     = "#282c33"
+theme.bg_focus      = "#394046"
+theme.bg_urgent     = "#ff5c57"
+theme.bg_minimize   = "#3a3f47"
+theme.fg_normal     = "#ffffff"
+theme.fg_focus      = "#ffffff"
+theme.fg_urgent     = "#ffffff"
+
+-- Borders & gaps
+theme.border_width  = 2
+theme.border_normal = "#22252b"
+theme.border_focus  = "#5b7bd6"
+theme.useless_gap   = 22
+
+-- Wibar (panel) appearance
+theme.wibar_height  = 40
+theme.wibar_bg      = "#282c33"
+theme.wibar_opacity = 0.85
+
+-- Wallpaper (absolute path)
+theme.wallpaper = "/home/foxcoder/Pictures/china.jpg"
+
+-- Awesome menu icon (absolute path)
+theme.awesome_icon = "/home/foxcoder/Pictures/arch.png"
+
+-- Informational / misc
+theme.cursor_shape = "beam" -- informational only; terminal handles actual cursor
+theme.icon_theme = nil
+
+return theme
